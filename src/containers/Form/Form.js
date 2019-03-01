e4d3b4485230febd8cf414c47f3cffac74ce8006
Lee Graham <graham.la3@gmail.com>
2019-02-28 21:12:05 -0700

    Create local state in Form

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -6,30 +6,63 @@ export class Form extends Component {
   constructor() {
     super()
     this.state = {
+      type: '',
       price: 0,
       volume: 0,
-      total: 0
+      total: 0,
     }
+  }
+
+  inputSubmit(e) {
+    e.preventDefault()
+    console.log(this.state)
+
+  }
+
+  updateState(e) {
+    const name = e.target.name
+    const value = e.target.value
+    this.setState({
+      [ name ]: value
+    })
+    
+    console.log(this.state)
   }
 
   render() {
 
     return(
-      <form>
-        <label for="dropdown">You Buying or Selling?</label>
-        <select id="dropdown">
-          <option value="buy">Buy</option>
-          <option value="sell">Sell</option>
+      <form onSubmit={e => this.inputSubmit(e)}>
+        <label htmlFor="dropdown">You Buying or Selling?</label>
+        <select id="dropdown" name="type" onChange={e => this.updateState(e)}>
+          <option >choose one</option>
+          <option value="buy" >Buy</option>
+          <option  value="sell" >Sell</option>
         </select>
 
-        <label for="price">Price?</label>
-        <input id="price" name="price" type="number" placeholder="000.00000"/>    
+        <label htmlFor="price">Price?</label>
+        <input id="price" 
+          name="price" 
+          type="number" 
+          placeholder="000.00000" 
+          onChange={e => this.updateState(e)}
+          value={this.state.price}/>    
 
-        <label for="volume">Volume?</label>
-        <input id="volume" name="volume" type="number" placeholder="000.00000"/>
+        <label htmlFor="volume">Volume?</label>
+        <input id="volume" 
+          name="volume" 
+          type="number" 
+          placeholder="000.00000" 
+          onChange={e => this.updateState(e)}
+          value={this.state.volume}/>
        
-        <label for="total">Total?</label>
-        <input id="total" name="total" type="number" placeholder="1 000.00000"/>
+        <label htmlFor="total">Total?</label>
+        <input id="total" 
+          name="total" 
+          type="number" 
+          placeholder="1 000.00000" 
+          onChange={e => this.updateState(e)}
+          value={this.state.total}/>
 
         <button>submit</button>
       </form>
